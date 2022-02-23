6378a3101144823157394e86beb24f3f10d61aa6
mmuthu <malathy_muthu@intuit.com>
2022-02-23 11:05:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,40 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='yellow'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
+shopping=['Woodlands Mall', 'College Park Mall', 'Yogaworks Mall', 'Outlet Factory']
+female_values=[30, 40, 25, 75]
+male_values=[20,25, 4, 40]
+color1='darkgreen'
+color2='darkred'
+mytitle='Visitor Counts'
+tabtitle='Shopping'
+myheading='Shopping Mall - Analytics'
+label1='Female'
+label2='Male'
+githublink='https://github.com/Malathy-Muthu/102-flying-dog-beers'
 sourceurl='https://www.flyingdog.com/beers/'
 
 ########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
+female_count = go.Bar(
+    x=shopping,
+    y=female_values,
     name=label1,
     marker={'color':color1}
 )
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
+male_count = go.Bar(
+    x=shopping,
+    y=male_values,
     name=label2,
     marker={'color':color2}
 )
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
+mall_data = [female_count, male_count]
+mall_layout = go.Layout(
     barmode='group',
     title = mytitle
 )
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+mall_fig = go.Figure(data=mall_data, layout=mall_layout)
 
 
 ########### Initiate the app
@@ -50,8 +50,8 @@ app.title=tabtitle
 app.layout = html.Div(children=[
     html.H1(myheading),
     dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+        id='Shopping',
+        figure=mall_fig
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
